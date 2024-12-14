85e19fda6f0d7fba830369382b2716b22dc99f8f
TC017 <thomas.cherian@gmail.com>
2024-12-13 21:27:54 -0500

    Task 3 complete

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,16 @@
 import requests  # Import the requests library to handle HTTP requests
+import json
 
-def emotion_detector (text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
+def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion analysis service
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+    max_emotion = max(emotion_dict, key=emotion_dict.get)
+    emotion_dict['dominant_emotion'] = max_emotion
+    
+    return emotion_dict  # Return the response text from the API
